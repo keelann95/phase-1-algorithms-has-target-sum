8f00f4215afcdc1c4cb049d7acdc9f4823f6a7e3
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-19 15:53:01 +0300

    modified js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
